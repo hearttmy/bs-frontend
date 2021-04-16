38cbb789017523201c088c1d801ddbdb1de01d34
unknown <1156609896@qq.com>
2021-04-16 16:19:05 +0800

    'v0.0'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+// import '@/permission' // permission control
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
