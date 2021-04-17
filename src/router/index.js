0243901a15325d2f544139bba8fbc09d5740c4bd
unknown <1156609896@qq.com>
2021-04-17 10:36:40 +0800

    'v0.1'

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Layout from '@/layout'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/user/Login')
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
   },
+
   {
     path: '/login',
     component: () => import('@/views/user/Login'),
     hidden: true
+  },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/Dashboard'),
+      meta: {
+        title: 'Dashboard',
+        icon: 'dashboard'
+      }
+    }]
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const createRouter = () => new VueRouter({
+  mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
   routes
 })
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter () {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
 
 export default router
